ab83b3d6284b505372ddcf164a596d2b6047f604
Christopher Gabriel <stuff@c-gabriel.at>
2012-02-01 12:47:32 +0100

    Cam 800x480 with paused reset

--- facedetection/app/include/i2c.h
+++ facedetection/app/include/i2c.h
@@ -5,6 +5,12 @@
 #define I2CCONFIG_STATUS (*(volatile int *const) (I2CCONFIG_BADDR))
 #define I2CCONFIG_DATA   (*(volatile int *const) (I2CCONFIG_BADDR+4))
 
-#define i2c_write(reg,word)	I2CCONFIG_STATUS = (((uint8_t) reg)<<16) | ((uint16_t) word)
+#define i2c_write(reg,word)	{ \
+	I2CCONFIG_STATUS = (((uint8_t) reg)<<16) | ((uint16_t) word); \
+	uint32_t j; \
+	for(j = 0; j < 1300; j++) asm volatile("nop\n\t"); \
+	}
+
+extern void initCamera(void);
 
 #endif /* I2C_H */
---
+++ facedetection/app/src/camera.c
@@ -0,0 +1,42 @@
+#include "i2c.h"
+
+void initCamera(void)
+{
+	/*
+ 	 *i2c config for camera, see hints.pdf
+ 	 */
+
+	// power pll
+	i2c_write(0x10,0x51);
+	// pll factor + divider
+	i2c_write(0x11,0x1404);			// 25MHz
+	/*
+	i2c_write(0x11,0x3004);
+	i2c_write(0x11,0x1e04);
+	*/
+	// pll p1 divider
+	i2c_write(0x12,0x01);
+	// use and power pll
+	i2c_write(0x10,0x53);
+	// invert pixel clock
+	i2c_write(0x0a,0x8000);
+	
+	// pause restart
+	i2c_write(0x0b,3);
+
+	// row size
+	i2c_write(0x03,1439);
+	// col size
+	i2c_write(0x04,2399);
+	// row skipping
+	i2c_write(0x22,2);
+	// col skipping
+	i2c_write(0x23,2);
+	// mirror rows
+	i2c_write(0x20,(1<<15));
+	// snapshot mode
+	i2c_write(0x1e,(0x4006 | (1<<8)));
+	
+	// unpause restart
+	i2c_write(0x0b,1);
+}
